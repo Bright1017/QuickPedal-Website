f6323043e598706c70248394b77c6b96cfe0b77d
Bright1017 <melvinbright101@gmail.com>
2025-10-02 16:23:46 +0100

    checking if users provided the necessary requirements to login

--- users/serializers.py
+++ users/serializers.py
@@ -3,13 +3,17 @@ from django.contrib.auth.password_validation import validate_password
 from django.core.mail import send_mail
 from .models import User
 
+
 class UserRegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'contact_number', 'password', 'password2', 'role']
+        fields = [
+            'username', 'first_name', 'last_name',
+            'email', 'contact_number', 'password', 'password2', 'role'
+        ]
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
@@ -19,12 +23,16 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         return attrs
 
     def create(self, validated_data):
-        validated_data.pop('password2')
-        user = User.objects.create_user(**validated_data)
-        user.is_active = False
-        user.generate_verification_code()
+        password = validated_data.pop("password")
+        validated_data.pop("password2")
 
-        # Send email with verification code
+        user = User.objects.create(**validated_data)
+        user.set_password(password)  # ensure password is hashed
+        user.is_active = False  # require verification
+        user.save()
+
+        # Generate and send verification code
+        user.generate_verification_code()
         send_mail(
             subject='Verify Your QuickPedal Account',
             message=f'Your verification code is: {user.verification_code}',
@@ -51,9 +59,10 @@ class VerifyCodeSerializer(serializers.Serializer):
         if user.is_code_expired():
             raise serializers.ValidationError("Verification code expired. Please request a new one.")
 
+        # Mark user as verified
         user.is_active = True
         user.is_verified = True
-        user.verification_code = ''
+        user.verification_code = None  # clear code
         user.code_generated_at = None
         user.save()
         return attrs
@@ -73,6 +82,10 @@ class ResendCodeSerializer(serializers.Serializer):
         if user.is_verified:
             raise serializers.ValidationError("This account is already verified.")
 
+        if not user.can_request_new_code():
+            raise serializers.ValidationError("Please wait before requesting a new code.")
+
+        # Generate and send a new verification code
         user.generate_verification_code()
         send_mail(
             subject='Your New QuickPedal Verification Code',
@@ -87,4 +100,8 @@ class ResendCodeSerializer(serializers.Serializer):
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'role', 'profile_photo', 'contact_number']
+        fields = [
+            'username', 'email', 'first_name', 'last_name',
+            'role', 'profile_photo', 'contact_number', 'is_verified'
+        ]
+
